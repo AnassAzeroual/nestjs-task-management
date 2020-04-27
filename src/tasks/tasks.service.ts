8af10e512b0952aa4241e3dad8ff6cdf9ec50cf1
Anass Azeroual <azeroual.anass2015@gmail.com>
2020-04-27 17:47:53 +0000

    add script debugger in vs code

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { TaskStatus, Task } from './tasks.model';
 import { v1 as uuidv1 } from 'uuid';
 import { SearchOrDeleteTaskDto, CreateTaskDto } from './tasks-dto';
@@ -26,14 +26,19 @@ export class TasksService {
         return this.tasks
     }
 
-    searchTask (deleteTaskID:SearchOrDeleteTaskDto)
+    searchTask (searchTaskID:SearchOrDeleteTaskDto)
     {
-        return this.tasks.find(x => x.id == deleteTaskID.id)
+        const res = this.tasks.find(x => x.id == searchTaskID.id.trim());
+        if (!res)
+        {
+            throw new NotFoundException(`No Result of This ID: ${searchTaskID.id}`)
+        }
+        return res
     }
 
     deleteTask (deleteTaskID:SearchOrDeleteTaskDto)
     {
-        this.tasks = this.tasks.filter(x => x.id != deleteTaskID.id)
+        this.tasks = this.tasks.filter(x => x.id != deleteTaskID.id.trim())
     }
 
 
